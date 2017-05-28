2158a0f6557d91939f3e20954e253c7508160c29
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-28 18:24:31 -0400

    add: Save and edit task

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,8 @@ class App extends Component {
          />
         <TodosList 
           todos={this.state.todos}  
-          toggleTask={this.toggleTask.bind(this)} 
+          toggleTask={this.toggleTask.bind(this)}
+          saveTask={this.saveTask.bind(this)} 
         />
       </div>
     );
@@ -53,6 +54,13 @@ class App extends Component {
       });
       this.setState({ todos: this.state.todos })
 
+  }
+
+  saveTask(oldTask, newTask) {
+    const foundTodo = _.find(this.state.todos, todo => todo.task === oldTask);
+
+    foundTodo.task = newTask; 
+    this.setState({ todos: this.state.todos });  
   }
 }
 
